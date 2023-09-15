f5f215b2bbb380c43863f9a87b9f0eafd90c056a
maurotapiadev <maurotapiadev@gmail.com>
2023-09-15 15:12:50 -0300

    ultimando detalles

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,8 +3,16 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer>
+      <div className='digitalhouse'>
         <p>Powered by</p>
-        <img src="./img/DH.png" alt='DH-logo'/>
+        <img src='../img/DH.png' alt='DH-logo' id='digital'/>
+      </div>  
+        <div className='redes'>
+          <img src="../img/ico-facebook.png" alt="FB-logo"/>
+          <img src="../img/ico-instagram.png" alt="IG-logo" />
+          <img src="../img/ico-tiktok.png" alt="TK-logo" />
+          <img src="../img/ico-whatsapp.png" alt="WSP-logo" />
+        </div>
     </footer>
   )
 }
--- src/Routes/Detail.jsx
+++ src/Routes/Detail.jsx
@@ -2,6 +2,7 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
 import { useDocStates } from '../Context/Context';
+import "../styles/Detail.css"
 
 
 const Detail = () => {
@@ -20,12 +21,16 @@ const Detail = () => {
   return (
     <>
     <div className='detail'>
+      <div>
         <h1>Detail Dentist id </h1>
         <h1>Nombre: {name}</h1>
-        <img src="/images/doctor.jpg" alt="" className="card-image"/>
         <h3>Email: {email}</h3>
         <h3>Telefono: {phone}</h3>
         <h3>Website: {website}</h3>
+        
+          <img src="/img/doctor.jpg" alt="" className="card-image"/>
+        </div>
+        
     </div>
     </>
   )
